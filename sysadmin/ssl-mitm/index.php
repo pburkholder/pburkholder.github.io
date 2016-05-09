4ad6d1ca9f331f20418958bb2187ab333dd3d8de
Peter Burkholder <pburkholder@chef.io>
2016-05-09 17:09:47 -0400

    Site updated at 2016-05-09 21:09:47 UTC

--- sysadmin/ssl-mitm/index.php
+++ sysadmin/ssl-mitm/index.php
@@ -1,8 +1,198 @@
+
 <!DOCTYPE html>
-<meta charset="utf-8">
-<title>Redirecting…</title>
-<link rel="canonical" href="/blog/2016/05/09/reviving-ssl-mitm-paper/">
-<meta http-equiv="refresh" content="0; url=/blog/2016/05/09/reviving-ssl-mitm-paper/">
-<h1>Redirecting…</h1>
-<a href="/blog/2016/05/09/reviving-ssl-mitm-paper/">Click here if you are not redirected.</a>
-<script>location="/blog/2016/05/09/reviving-ssl-mitm-paper/"</script>
+<!--[if IEMobile 7 ]><html class="no-js iem7"><![endif]-->
+<!--[if lt IE 9]><html class="no-js lte-ie8"><![endif]-->
+<!--[if (gt IE 8)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html class="no-js" lang="en"><!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <title>Index - A Blog</title>
+  <meta name="author" content="Peter Burkholder">
+
+  
+  <meta name="description" content="Index May 9th, 2016 1:56 pm May 9th, 2016 1:56 pm Tweet Comments Please enable JavaScript to view the comments powered by Disqus. Recent Posts &hellip;">
+  
+
+  <!-- http://t.co/dKP3o1e -->
+  <meta name="HandheldFriendly" content="True">
+  <meta name="MobileOptimized" content="320">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  
+  <link rel="canonical" href="http://pburkholder.github.io/sysadmin/ssl-mitm/index.php">
+  <link href="/favicon.png" rel="icon">
+  <link href="/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css">
+  <link href="/atom.xml" rel="alternate" title="A Blog" type="application/atom+xml">
+  <script src="/javascripts/modernizr-2.0.js"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  <script>!window.jQuery && document.write(unescape('%3Cscript src="/javascripts/libs/jquery.min.js"%3E%3C/script%3E'))</script>
+  <script src="/javascripts/octopress.js" type="text/javascript"></script>
+  <!--Fonts from Google"s Web font directory at http://google.com/webfonts -->
+<link href="//fonts.googleapis.com/css?family=PT+Serif:regular,italic,bold,bolditalic" rel="stylesheet" type="text/css">
+<link href="//fonts.googleapis.com/css?family=PT+Sans:regular,italic,bold,bolditalic" rel="stylesheet" type="text/css">
+
+  
+
+</head>
+
+<body   >
+  <header role="banner"><hgroup>
+  <h1><a href="/">A Blog</a></h1>
+  
+    <h2>An occasional data tap into Peter Burkholder&#8217;s brain</h2>
+  
+</hgroup>
+
+</header>
+  <nav role="navigation"><ul class="subscription" data-subscription="rss">
+  <li><a href="/atom.xml" rel="subscribe-rss" title="subscribe via RSS">RSS</a></li>
+  
+</ul>
+  
+<form action="https://www.google.com/search" method="get">
+  <fieldset role="search">
+    <input type="hidden" name="sitesearch" value="pburkholder.github.io">
+    <input class="search" type="text" name="q" results="0" placeholder="Search"/>
+  </fieldset>
+</form>
+  
+<ul class="main-navigation">
+  <li><a href="/">Blog</a></li>
+  <li><a href="/blog/archives">Archives</a></li>
+</ul>
+
+</nav>
+  <div id="main">
+    <div id="content">
+      <div>
+<article role="article">
+  
+  <header>
+    <h1 class="entry-title">Index</h1>
+    <p class="meta">
+
+
+
+
+<time class='entry-date' datetime='2016-05-09T13:56:00-04:00'><span class='date'><span class='date-month'>May</span> <span class='date-day'>9</span><span class='date-suffix'>th</span>, <span class='date-year'>2016</span></span> <span class='time'>1:56 pm</span></time></p>
+  </header>
+  
+  
+  
+    <footer>
+      <p class="meta">
+        
+        
+
+
+
+
+<time class='entry-date' datetime='2016-05-09T13:56:00-04:00'><span class='date'><span class='date-month'>May</span> <span class='date-day'>9</span><span class='date-suffix'>th</span>, <span class='date-year'>2016</span></span> <span class='time'>1:56 pm</span></time>
+        
+      </p>
+      
+        <div class="sharing">
+  
+  <a href="//twitter.com/share" class="twitter-share-button" data-url="http://pburkholder.github.io/sysadmin/ssl-mitm/index.php" data-via="pburkholder" data-counturl="http://pburkholder.github.io/sysadmin/ssl-mitm/index.php" >Tweet</a>
+  
+  
+  
+</div>
+
+      
+    </footer>
+  
+</article>
+
+  <section>
+    <h1>Comments</h1>
+    <div id="disqus_thread" aria-live="polite"><noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+</div>
+  </section>
+
+</div>
+
+<aside class="sidebar">
+  
+    <section>
+  <h1>Recent Posts</h1>
+  <ul id="recent_posts">
+    
+      <li class="post">
+        <a href="/blog/2016/05/09/reviving-ssl-mitm-paper/">Reviving My SSL Man-in-the-Middle Paper</a>
+      </li>
+    
+      <li class="post">
+        <a href="/blog/2016/03/24/windows-chef-client-log-rotation/">Windows Chef-client Log Rotation</a>
+      </li>
+    
+      <li class="post">
+        <a href="/blog/2016/03/04/chef-custom-resources-heres-an-easy-example/">Chef Custom Resources: Here&#8217;s an Easy Example</a>
+      </li>
+    
+      <li class="post">
+        <a href="/blog/2016/02/03/lambda-to-defer-chef-code-evaluation/">Lambda to Defer Chef Code Evaluation</a>
+      </li>
+    
+      <li class="post">
+        <a href="/blog/2016/01/29/6-ways-to-resolve-chef-cookbook-version-conflicts/">6 Ways to Resolve Chef Cookbook Version Conflicts</a>
+      </li>
+    
+  </ul>
+</section>
+
+
+
+
+
+  
+</aside>
+
+
+    </div>
+  </div>
+  <footer role="contentinfo"><p>
+  Copyright &copy; 2016 - Peter Burkholder -
+  <span class="credit">Powered by <a href="http://octopress.org">Octopress</a></span>
+</p>
+
+</footer>
+  
+
+<script type="text/javascript">
+      var disqus_shortname = 'blogpburkholdercom';
+      
+        
+        // var disqus_developer = 1;
+//        var disqus_identifier = 'http://pburkholder.github.io/sysadmin/ssl-mitm/index.php';
+//        var disqus_url = 'http://pburkholder.github.io/sysadmin/ssl-mitm/index.php';
+        var disqus_script = 'embed.js';
+      
+    (function () {
+      var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+      dsq.src = '//' + disqus_shortname + '.disqus.com/' + disqus_script;
+      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    }());
+</script>
+
+
+
+
+
+
+
+  <script type="text/javascript">
+    (function(){
+      var twitterWidgets = document.createElement('script');
+      twitterWidgets.type = 'text/javascript';
+      twitterWidgets.async = true;
+      twitterWidgets.src = '//platform.twitter.com/widgets.js';
+      document.getElementsByTagName('head')[0].appendChild(twitterWidgets);
+    })();
+  </script>
+
+
+
+
+
+</body>
+</html>
